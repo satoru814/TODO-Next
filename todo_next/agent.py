fd9013e0d15f139702c4991a334726d81b79709d
satoru814 <satoru-sakamoto@c-fo.com>
2023-03-30 21:46:03 +0900

    refine prompt

--- todo_next/agent.py
+++ todo_next/agent.py
@@ -5,13 +5,30 @@ from langchain import LLMChain, OpenAI, PromptTemplate
 
 
 def get_now():
-    return str(datetime.datetime.now())
-    
+    time = datetime.datetime.now()
+    return time.strftime('%Y年%m月%d日 %H時%M分')
+
+def task_list():
+    with open("task.json", "r") as f:
+        tasks = json.load(f)["task"]
+        tasks = "\n".join(tasks)
+    print(tasks)
+
+
+def task_sort():
+    with open("task.json", "r") as f:
+        tasks = json.load(f)["task"]
+        sorted_tasks = sort_task(tasks)
+    with open("task.json", "w") as f:
+        f.write(json.dumps({"task":sorted_tasks}, ensure_ascii=False))
+
+
+
 def task_push():
     new_task = input("Hi! What's your task:\n")
     comment = input("""I got it! You can write comment(exp. tomorrow night is deadline, This is difficult and so on)\n""")
     now = get_now()
-    task_describe = f"Task title:{new_task}/User comment:{comment}/Push time:{now}"
+    task_describe = f"タスク内容:{new_task}/いつまでにやりたいか:{comment}/登録日時{now}"
 
     with open("task.json", "r") as f:
         current_tasks = json.load(f)["task"]
@@ -20,34 +37,40 @@ def task_push():
     with open("task.json", "w") as f:
         f.write(json.dumps({"task":sorted_tasks}, ensure_ascii=False))
 
+
     
 def sort_task(tasks):
     llm = OpenAI(temperature=0, model="text-davinci-003")
     template="""
+    現在の日時は{now}です。
     あなたは私の秘書です。今私のタスクは次のようになっています。
-    現在の日付と時刻は{now}です。
-    各タスクの重要度や作業時間などを考慮して優先度の高い順番に並び替えてください。
-    Task titleはタスクの内容を示しています。
-    User commentはユーザーがそのタスクについて考えていることです。
-    Push timeはユーザーがそのタスクを登録した時刻です。
+    各タスクは
+    タスク番号. タスク内容/いつまでにやりたいか/登録日時
+    のように与えられています。
+    
+    {tasks}
 
-    例
-    現在のタスクのPython list: ["テスト勉強をする", "ドレッシングを買いに行く", "中期の目標を立てる。"]
-    並び替えた後のタスク:
-    中期の目標を立てる。
-    テスト勉強をする
-    ドレッシングを買いに行く
+    各タスクの重要度、登録日時、現在の日時、コメントなどを総合的に評価して優先度の高い順番に並び替えて出力してください。
+    出力する際には以下に示す例のようにタスク番号を出力してください。
+    例: [1,3,5,4,2]
 
-    現在の実行
-    現在のタスクのPython list: {tasks}
-    並び替えた後のタスク:
+    出力する際には以下の制約を守ってください
+    1. 1~{tasks_num}までの番号を１回づつ出力する
+
+    では始めてください
+    出力:
     """
     prompt = PromptTemplate(template=template, 
-                            input_variables=["tasks", "now"])
+                            input_variables=["tasks", "tasks_num", "now"])
     bot = LLMChain(llm=llm, prompt=prompt, verbose=True)
-    sorted_tasks = bot.predict(tasks=str(tasks), now=get_now())
-    sorted_tasks = sorted_tasks.split("\n")[1:]
-    print(sorted_tasks)
+    gpt_tasks = ""
+    for i, task in enumerate(tasks):
+        gpt_tasks += f"{i+1}. {task}\n"
+    sorted_number = bot.predict(tasks=str(gpt_tasks), now=get_now(), tasks_num=len(tasks))
+    sorted_number = ast.literal_eval(sorted_number)
+    sorted_tasks = []
+    for i in sorted_number:
+        sorted_tasks.append(tasks[int(i)-1])
     return sorted_tasks
 
 # def main():
